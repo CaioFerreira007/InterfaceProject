ea9fa0fd80b036cab8120f06626f31c2db9e6127
Caio Ferreira <147638926+CaioFerreira007@users.noreply.github.com>
2025-06-26 13:53:48 -0300

    Add files via upload

---
+++ Services/RentalServices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Interfaces.Entities;
+using System.Threading.Tasks;
+
+namespace Interfaces.Services
+{
+    class RentalServices
+    {
+        public double PricePerHour { get;private set; }
+        public double PricePerDay { get;private set; }
+
+        private ITaxServices _taxService;
+
+        public RentalServices(double pricePerHour, double pricePerDay, ITaxServices taxServices)
+        {
+            PricePerHour = pricePerHour;
+            PricePerDay = pricePerDay;
+            _taxService = taxServices;
+        }
+
+        public void ProcessInvoice(CarRental carRental)
+        {
+            TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+            double basicPayment = 0.0;
+            if(duration.TotalHours <= 12)
+            {
+                basicPayment = PricePerHour * Math.Ceiling(duration.TotalHours);
+            }
+            else
+            {
+                basicPayment = PricePerDay * Math.Ceiling(duration.TotalDays);
+            }
+            double tax = _taxService.Tax(basicPayment);
+            carRental.Invoice = new Invoice(basicPayment, tax);
+        }
+
+    }
+}
